409b725460222f3dd27f7199057daf3dbabd89a1
Aleksey Maksimenko <alexward2096@gmail.com>
2023-01-11 20:09:31 +0300

    test

--- task04sql/src/main/java/by/academy/task04sql/service/CollectionPrinter.java
+++
@@ -1,9 +0,0 @@
-package by.academy.task04sql.service;
-
-import java.util.Collection;
-
-public final class CollectionPrinter {
-	public <T> void printCollection(final Collection<T> collection) {
-		collection.forEach(System.out::println);
-	}
-}
